9eb104d967bb56e67fc19211aed98f8288cd0e20
Anuoluwapo <63651250+Owoade@users.noreply.github.com>
2022-11-26 10:18:52 +0100

    Migrated API from heroku to railway

--- build/sdk.js
+++ build/sdk.js
@@ -1,4 +1,6 @@
 "use strict";
+let clicked = false;
+
 class Once {
     constructor(payload) {
         this.payload = payload !== null && payload !== void 0 ? payload : {};
@@ -19,18 +21,28 @@ class Once {
         return { missingPayload: MISSING_PAYLOAD, typeMismatch: TYPE_MISMATCH };
     }
     async checkout() {
+
+        if( clicked ) return console.error("Too many clicks, pop-up already active!");
+
+        clicked = true;
+
         const errors = this.validatePayload();
         //   No errors
         const NO_ERROR = Object.values(errors).every((error) => !Boolean(error));
+
         if (NO_ERROR) {
             const checkout = await this.getCheckoutLink();
+            this.setUpEvents( checkout.transaction_ref );
             window.open(checkout.url, "New Window", `width=500,height=700,top=${(screen.height - 700) / 4},left=${(screen.width - 500) / 2}`);
             return;
         }
+
         Boolean(errors.missingPayload) &&
             console.error(`Payload object must contain '${errors.missingPayload}' property `);
+
         Boolean(errors.typeMismatch) &&
             console.error(`${errors.typeMismatch} must be of type ${errors.typeMismatch === "amount" ? "number" : "function"}`);
+
     }
     async getCheckoutLink() {
         const requestConfig = {
@@ -40,7 +52,23 @@ class Once {
             },
             body: JSON.stringify({ amount: this.payload.amount, host: window.location.host }),
         };
+        clicked = false;
         const res = await fetch("https://web-production-133c.up.railway.app/init", requestConfig);
         return await res.json();
+    }
+
+    setUpEvents( ref ){
+        try{
+            const socket = io("https://web-production-133c.up.railway.app/transaction");
+
+            console.log( ref );
+
+            socket.emit("transaction-init", ref );
+
+            socket.on("transaction-resolved", this.payload.successCallback );
+        }catch( e ){
+            throw new Error("socket-io client cdn script missing, use <script src='https://cdn.socket.io/4.5.3/socket.io.min.js' ></script> preceeding all other scripts ");
+        }
+        
     }
 }
